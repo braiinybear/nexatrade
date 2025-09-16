0e1ade9523b94f415463737578c73f280a992b57
braiinybear <braiinybear@gmail.com>
2025-09-16 16:03:50 +0530

    changing name

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { AuthProvider } from "@/context/AuthContext";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "NexaTrade",
+  title: "OctaFx",
   description: "Trading platform",
 };
 
